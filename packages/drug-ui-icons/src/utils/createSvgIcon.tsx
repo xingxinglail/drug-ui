8e8956b3ef51c5f57790d20a026d8657aa950588
har <istarcoming@163.com>
2019-12-03 18:04:36 +0800

    fix: 知道返回类型
    affects: @drug-ui/icons

--- packages/drug-ui-icons/src/utils/createSvgIcon.tsx
+++ packages/drug-ui-icons/src/utils/createSvgIcon.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { SvgIcon, SvgIconProps } from '@drug-ui/core';
 
-const createSvgIcon = (path: React.ReactNode, name: string) => {
+const createSvgIcon = (path: React.ReactNode, name: string): typeof SvgIcon => {
     const Component = React.forwardRef<SVGSVGElement, SvgIconProps>((props, ref) => (
         React.createElement(SvgIcon, { ref, ...props }, path)
     ));
