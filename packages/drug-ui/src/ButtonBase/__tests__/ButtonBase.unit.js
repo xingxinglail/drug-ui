e576bbf470bda53ebbcbe6b085506eb5479cccbd
har <istarcoming@163.com>
2019-12-11 10:03:32 +0800

    test(ButtonBase): 修改描述
    affects: @drug-ui/core

--- packages/drug-ui/src/ButtonBase/__tests__/ButtonBase.unit.js
+++ packages/drug-ui/src/ButtonBase/__tests__/ButtonBase.unit.js
@@ -2,8 +2,6 @@ import * as renderer from 'react-test-renderer';
 import * as React from 'react';
 import * as enzyme from 'enzyme';
 import ButtonBase from '..';
-import Button from '../../Button';
-import { Wrapper } from '@drug-ui/core/test-utils';
 
 const mount = enzyme.mount;
 
@@ -14,33 +12,26 @@ describe('<ButtonBase />', () => {
         expect(json).toMatchSnapshot();
     });
 
-    it('set props className', () => {
+    it('可以设置 className', () => {
         const wrapper = mount(<ButtonBase className="button button2" />);
         expect(wrapper.hasClass('button button2')).toBeTruthy();
     });
 
-    it('set props href', () => {
+    it('可以设置 href', () => {
         const wrapper = mount(<ButtonBase href="https://google.com" />);
         const dom = wrapper.getDOMNode();
         expect(dom.tagName).toBe('A');
         expect(dom.getAttribute('href')).toBe('https://google.com');
     });
 
-    it('span set props href', () => {
+    it('为 span 设置 href', () => {
         const wrapper = mount(<ButtonBase component="span" href="https://google.com" />);
         const dom = wrapper.getDOMNode();
         expect(dom.tagName).toBe('SPAN');
         expect(dom.getAttribute('href')).toBe('https://google.com');
     });
 
-    it('set props component href to aLink', () => {
-        const wrapper = mount(<ButtonBase href="https://google.com" />);
-        const dom = wrapper.getDOMNode();
-        expect(dom.tagName).toBe('A');
-        expect(dom.getAttribute('href')).toBe('https://google.com');
-    });
-
-    it('set props component', () => {
+    it('可以传入组件', () => {
         const Comp = ((props) => {
             const { children, ...rest } = props;
             return <a href="https://baidu.com" { ...rest }>{ children }</a>;
@@ -51,7 +42,7 @@ describe('<ButtonBase />', () => {
         expect(dom.getAttribute('href')).toBe('https://baidu.com');
     });
 
-    it('set props disabled', () => {
+    it('可以设置 disabled', () => {
         const fn = jest.fn();
         const wrapper = mount(<ButtonBase disabled onClick={ fn } />);
         const dom = wrapper.getDOMNode();
@@ -60,20 +51,10 @@ describe('<ButtonBase />', () => {
         expect(fn).not.toBeCalled();
     });
 
-    it('set props ripple', () => {
+    it('可以设置 ripple', () => {
         const wrapper = mount(<ButtonBase />);
         expect(wrapper.find('span').exists()).toBeTruthy();
         wrapper.setProps({ disableRipple: true });
         expect(wrapper.find('span').exists()).toBeFalsy();
-    });
-
-    // todo events
-    it('events', () => {
-        const wrapper = mount((
-            <Wrapper>
-                <Button>Click Me</Button>
-            </Wrapper>
-        ))
-        wrapper.find('.DuiRipple-root').simulate('mousedown');
     });
 });
